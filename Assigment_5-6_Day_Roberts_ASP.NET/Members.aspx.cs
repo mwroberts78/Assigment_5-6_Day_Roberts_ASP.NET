27ba90fe9b5a3e239520c7df19d69376bb593e50
Justin <101442943+Day-Justin@users.noreply.github.com>
2025-11-26 06:16:34 -0500

    Added methods to edit points for both members and staff

--- Assigment_5-6_Day_Roberts_ASP.NET/Members.aspx.cs
+++ Assigment_5-6_Day_Roberts_ASP.NET/Members.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.SqlServer.Server;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -25,6 +26,106 @@ namespace Assigment_5_6_Day_Roberts_ASP.NET
                     lblLoggedIn.Text = "Logged in as: <strong>" + username + "</strong>";
                 }
             }
+        }
+
+        protected string Redeem(string cost) 
+        {
+            /* 
+             * Assumes that the user is logged in and their info is in session
+             * to reach have access to reach this method.
+             * needs to cost parameter of reward to input
+             */
+            // Redemption logic to for points goes here
+            // Will either be successful or show difference in points needed
+            string result = "";
+
+            var user = Session["member_user"] as System.Xml.Linq.XElement;
+            string username = "";
+            string points = "";
+            if (user != null)
+            {
+                username = (string)user.Element("email");
+                points = (string)user.Element("points");
+            }
+            else 
+            {
+                result = "User not found.";
+                return result;
+            }
+
+            if (int.Parse(points) < int.Parse(cost))
+            {
+                // if insufficient points
+                string difference = (int.Parse(cost) - int.Parse(points)).ToString();
+                result = "Unsuccessful. Insufficient by " + difference + " points.";
+            }
+            else
+            {
+                // successful redemption
+                // update on session and db before returning success message
+                string newPoints = (int.Parse(points) - int.Parse(cost)).ToString();
+                result = "Redemption successful! You have " + newPoints + " points remaining.";
+
+                user.SetElementValue("points", newPoints);
+                Session["member_user"] = user;
+
+                // update points in XML database
+                string xmlPath = Server.MapPath("~/App_Data/members.xml");
+                System.Xml.Linq.XDocument doc = System.Xml.Linq.XDocument.Load(xmlPath);
+                var userInDb = doc.Descendants("member")
+                    .FirstOrDefault(x => ((string)x.Element("email")).ToLower() == username.ToLower());
+                if (userInDb != null)
+                {
+                    userInDb.SetElementValue("points", newPoints);
+                    doc.Save(xmlPath);
+                }
+                else
+                {
+                    result = "Error updating points in database.";
+                    return result;
+
+                }
+            }
+
+            return result;
+        }
+
+        protected string GetShippingCost(string zip, string weight)
+        {
+            // items need shipping cost calculation based on zip code and weight
+            // zip code is user provided and cost and weight are from the item being shipped
+
+            string result = "";
+            try
+            {
+                var client = new Day_Roberts_Service.Service1Client();
+                result = client.EstimateShipping(zip, double.Parse(weight)).ToString("C");
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                result = $"Error: {ex.Message}";
+            }
+
+            return result;
+        }
+
+        protected string GetSalesTax(string zip, string cost)
+        {
+            // items need sales tax calculation based on zip code and cost of items
+            // zip code is user provided and cost is from the item being purchased
+            string result = "";
+            try
+            {
+                var client = new Day_Roberts_Service.Service1Client();
+                result = client.GetSalesTax(zip, double.Parse(cost)).ToString("C");
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                result = $"Error: {ex.Message}";
+            }
+            return result;
         }
     }
 }
--- Assigment_5-6_Day_Roberts_ASP.NET/Staff.aspx.cs
+++ Assigment_5-6_Day_Roberts_ASP.NET/Staff.aspx.cs
@@ -26,5 +26,41 @@ namespace Assigment_5_6_Day_Roberts_ASP.NET
                 }
             }
         }
-    }
+
+        protected string UpdatePoints(string username, string points)
+        {
+            // method to update member points from staff page
+            // takes in username and points to update
+            // will only be available if staff user is logged in
+
+            string result = "";
+
+            if (points != null && username != null)
+            {
+                string xmlPath = Server.MapPath("~/App_Data/members.xml");
+                System.Xml.Linq.XDocument doc = System.Xml.Linq.XDocument.Load(xmlPath);
+                var userInDb = doc.Descendants("member")
+                    .FirstOrDefault(x => ((string)x.Element("email")).ToLower() == username.ToLower());
+                if (userInDb != null)
+                {
+                    userInDb.SetElementValue("points", points);
+                    doc.Save(xmlPath);
+                }
+                else
+                {
+                    result = "Error updating points in database.";
+                    return result;
+
+                }
+
+            }
+            else 
+            {
+                result = "Username or points missing.";
+                return result;
+            }
+
+            return result;
+
+        }
 }
